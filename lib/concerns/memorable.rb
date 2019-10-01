ef4271586c214449bb9f6ec164b56ce4634a30eb
Micah Levine <micah.levine@outlook.com>
2019-10-01 01:08:14 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,19 @@
 module Memorable
+  module ClassMethods
 
   def reset_all
-    all.clear
+    self.all.clear
   end
 
   def count
-    all.count
+    self.all.count
   end
 end
+
+module InstanceMethods
+  def initialize
+    self.class.all << self
+  end
+end
+end
+
